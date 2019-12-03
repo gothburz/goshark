127f11d7f3922683a7898e9cf3ea04e0dd246431
gothburz <peter.girnus@gmail.com>
2019-12-03 15:27:59 -0600

    added get uri params arg

--- goshark.go
+++ goshark.go
@@ -21,6 +21,14 @@ var (
 	// get HOST
 	getHostCommand = getCommand.Command("host", "HOST Command")
 	hostPCAP       = getHostCommand.Arg("PCAP File", "PCAP to extract Host(s) from.").Required().String()
+
+	// get USER-AGENT
+	getUserAgentCommand = getCommand.Command("user-agent", "USER-AGENT Command")
+	userAgentPCAP       = getUserAgentCommand.Arg("PCAP FIle", "PCAP to extract Host(s) from.").Required().String()
+
+	// get URI PARAMS
+	getURIParamsCommand = getCommand.Command("uri-params", "URI PARAMETER Command")
+	uriParamsPCAP       = getURIParamsCommand.Arg("PCAP File", "PCAP to extract URI Parameter(s) from.").Required().String()
 )
 
 func getPCAPPath(relativePath string) (pcapPath string) {
@@ -68,6 +76,39 @@ func main() {
 				log.Fatalf("goshark failed with %s\n", err)
 			}
 		}
+	// GET USER-AGENT CASE
+	case getUserAgentCommand.FullCommand():
+		if userAgentPCAP != nil {
+			pcapPath := getPCAPPath(*userAgentPCAP)
+			tshark := exec.Command("tshark", "-r"+pcapPath,
+				"-Y", "(http.request) && (tcp.stream)",
+				"-T", "fields",
+				"-e", "tcp.stream",
+				"-e", "http.user_agent",
+				"-E", "separator=/s")
+			tshark.Stdout = os.Stdout
+			tshark.Stderr = os.Stderr
+			err := tshark.Run()
+			if err != nil {
+				log.Fatalf("goshark failed with %s\n", err)
+			}
+		}
+	// GET URI PARAMETERS CASE
+	case getURIParamsCommand.FullCommand():
+		if uriParamsPCAP != nil {
+			pcapPath := getPCAPPath(*uriParamsPCAP)
+			tshark := exec.Command("tshark", "-r"+pcapPath,
+				"-Y", "(http.request) && (tcp.stream)",
+				"-T", "fields",
+				"-e", "tcp.stream",
+				"-e", "http.request.uri.query.parameter",
+				"-E", "separator=/s")
+			tshark.Stdout = os.Stdout
+			tshark.Stderr = os.Stderr
+			err := tshark.Run()
+			if err != nil {
+				log.Fatalf("goshark failed with %s\n", err)
+			}
+		}
 	}
-
 }
