4887fa77257914e621b634e6b6cfb88a009ce86d
gothburz <peter.girnus@gmail.com>
2019-12-03 15:02:31 -0600

    added get host case for http requests.

--- goshark.go
+++ goshark.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"gopkg.in/alecthomas/kingpin.v2"
 	"log"
 	"os"
@@ -12,26 +11,31 @@ import (
 var (
 	// COMMAND-LINE ARGUMENTS
 	app = kingpin.New("goshark", "golang pcap manipulation application.")
-	// GET
-	// - URI
-	getCommand    = app.Command("get", "Get something from a PCAP.")
-	getURICommand = getCommand.Command("uri", "URI Subcommand.")
+
+	getCommand = app.Command("get", "Get something from a PCAP.")
+
+	// get URI
+	getURICommand = getCommand.Command("uri", "URI Command")
 	uriPCAP       = getURICommand.Arg("PCAP File", "PCAP to extract URI(s) from.").Required().String()
+
+	// get HOST
+	getHostCommand = getCommand.Command("host", "HOST Command")
+	hostPCAP       = getHostCommand.Arg("PCAP File", "PCAP to extract Host(s) from.").Required().String()
 )
 
 func getPCAPPath(relativePath string) (pcapPath string) {
-	pcapPath, err := filepath.Abs(*uriPCAP)
+	pcapPath, err := filepath.Abs(relativePath)
 	if err != nil {
 		log.Fatal(err)
 	}
-	return
+	return pcapPath
 }
 
 func main() {
+	app.HelpFlag.Short('h')
 	switch kingpin.MustParse(app.Parse(os.Args[1:])) {
-	// EXTRACT URI CASE
+	// GET URI CASE
 	case getURICommand.FullCommand():
-		fmt.Println(app.Name, getURICommand.FullCommand())
 		if uriPCAP != nil {
 			pcapPath := getPCAPPath(*uriPCAP)
 			tshark := exec.Command("tshark", "-r"+pcapPath,
@@ -47,5 +51,23 @@ func main() {
 				log.Fatalf("goshark failed with %s\n", err)
 			}
 		}
+	// GET HOST CASE
+	case getHostCommand.FullCommand():
+		if hostPCAP != nil {
+			pcapPath := getPCAPPath(*hostPCAP)
+			tshark := exec.Command("tshark", "-r"+pcapPath,
+				"-Y", "(http.request) && (tcp.stream)",
+				"-T", "fields",
+				"-e", "tcp.stream",
+				"-e", "http.host",
+				"-E", "separator=/s")
+			tshark.Stdout = os.Stdout
+			tshark.Stderr = os.Stderr
+			err := tshark.Run()
+			if err != nil {
+				log.Fatalf("goshark failed with %s\n", err)
+			}
+		}
 	}
+
 }
